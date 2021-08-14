0dfd06e860df0a55dbb7fe0aaae9d8983382f932
JunFe23 <ogum0405@hanmail.net>
2021-08-14 22:46:57 +0900

    test

--- src/mymind/Lotto.java
+++ src/mymind/Lotto.java
@@ -1,5 +1,23 @@
 package mymind;
 
 public class Lotto {
-
+	public static void main(String[] args) {
+		int lotto[] = new int[6];
+		
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i]=(int)(Math.random()*45)+1;
+			
+			for (int j = 0; j < i; j++) {
+				if(lotto[i]==lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+		System.out.print("이번주 철또 추천번호 : ");
+		
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.print(lotto[i]+" ");
+		}
+	}
 }
