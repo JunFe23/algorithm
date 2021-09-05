e243539d17296affe65fe7c5c082c10b0a56a8f3
JunFe23 <ogum0405@hanmail.net>
2021-09-05 23:37:30 +0900

    2021.09.06

--- src/algorithm/practice/array/Q4.java
+++ src/algorithm/practice/array/Q4.java
@@ -1,10 +1,14 @@
 package algorithm.practice.array;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Q4 {
 	public static void main(String[] args) {
+		
+		// #1
 		Scanner sc = new Scanner(System.in);
 		HashSet<Integer> h = new HashSet<Integer>();
 		
@@ -14,5 +18,7 @@ public class Q4 {
 		
 		sc.close();
 		System.out.println(h.size());
+		
+		
 	}
 }
