897ca14063a4a813db71ea8df5d984cecba6170a
JunFe23 <ogum0405@hanmail.net>
2021-09-08 23:43:50 +0900

    2021.09.08

--- src/algorithm/practice/array/Q5.java
+++ src/algorithm/practice/array/Q5.java
@@ -1,5 +1,28 @@
 package algorithm.practice.array;
 
-public class Q5 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Q5 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		double arr[] = new double[in.nextInt()];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=in.nextDouble();
+		}
+		in.close();
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < arr.length; i++) {
+			sum += ((arr[i]/arr[arr.length-1])*100);
+		}
+		
+		System.out.println(sum/arr.length);
+		
+		
+	}
 }
