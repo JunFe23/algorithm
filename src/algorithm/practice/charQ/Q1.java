2b27111f0fe6251c45559fcff289467ef0f6e70d
JunFe23 <ogum0405@hanmail.net>
2021-09-15 23:42:45 +0900

    2021.09.15 문자열 시작.

---
+++ src/algorithm/practice/charQ/Q1.java
@@ -0,0 +1,15 @@
+package algorithm.practice.charQ;
+
+import java.util.Scanner;
+
+public class Q1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String s = in.next();
+		char a = s.charAt(0);
+		int b = (int)a;
+		
+		System.out.println(b);
+	}
+}
--- src/algorithm/practice/function/Q2.java
+++ src/algorithm/practice/function/Q2.java
@@ -11,7 +11,7 @@ public class Q2 {
 				check[n] = true;
 			}
 		}
-		
+	
 		StringBuilder sb = new StringBuilder();
 		
 		for (int i = 1; i < 10001; i++) {
