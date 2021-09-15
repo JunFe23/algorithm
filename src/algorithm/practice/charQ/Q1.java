5c9f9e3eeef03c91bc78eb965f0194cf24b9eb0d
JunFe23 <ogum0405@hanmail.net>
2021-09-15 23:46:21 +0900

    2021.09.15 Scanner 사용하지 않고 기초입력방법인 System.in 사용.

--- src/algorithm/practice/charQ/Q1.java
+++ src/algorithm/practice/charQ/Q1.java
@@ -3,13 +3,16 @@ package algorithm.practice.charQ;
 import java.util.Scanner;
 
 public class Q1 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+//		Scanner in = new Scanner(System.in);
+//		
+//		String s = in.next();
+//		char a = s.charAt(0);
+//		int b = (int)a;
+//		
+//		System.out.println(b);
 		
-		String s = in.next();
-		char a = s.charAt(0);
-		int b = (int)a;
-		
-		System.out.println(b);
+		int a = System.in.read();
+		System.out.println(a);
 	}
 }
