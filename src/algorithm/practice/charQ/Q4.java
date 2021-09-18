928abbf002e459c3fbdcd2ebf1e578acfc6d1281
JunFe23 <ogum0405@hanmail.net>
2021-09-19 01:17:52 +0900

    2021.09.18 문자열 반복 해결.

--- src/algorithm/practice/charQ/Q4.java
+++ src/algorithm/practice/charQ/Q4.java
@@ -9,29 +9,25 @@ public class Q4 {
 		int testcase = in.nextInt();
 		
 
-		try {
-			for (int i = 0; i < testcase; i++) {
-				int n = in.nextInt();
-				String input = in.next();
-				
-				char[] c = new char[input.length()];
-				
-				for (int j = 0; j < c.length; j++) {
-					c[j] = input.charAt(j);
-				}
-				
-				for (int j = 0; j < input.length()*n; j++) {
-					int cnt = 0;
-					while(cnt<n) {
-						System.out.print(c[j]);
-						cnt++;
-					}
-				}
+		for (int i = 0; i < testcase; i++) {
+			int n = in.nextInt();
+			String input = in.next();
+			
+			char[] c = new char[input.length()];
+			
+			for (int j = 0; j < c.length; j++) {
+				c[j] = input.charAt(j);
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+			
+			for (int j = 0; j < input.length(); j++) {
+				int cnt = 0;
+				while(cnt<n) {
+					System.out.print(c[j]);
+					cnt++;
+				}
+				
+			}
+			System.out.println();
 		}
-		
 	}
 }
