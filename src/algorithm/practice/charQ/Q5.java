96c7a9b0623da5bb77433750df1c6e308ed9bd94
JunFe23 <ogum0405@hanmail.net>
2021-09-21 20:47:50 +0900

    2021.09.21

--- src/algorithm/practice/charQ/Q5.java
+++ src/algorithm/practice/charQ/Q5.java
@@ -6,14 +6,30 @@ public class Q5 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		
+		int[] alpha = new int[26];
 		String m = in.next();
 		
-		char[] c = new char[m.length()];
-		
-		char[] alpha = new char[26];
-		
-		for (int i = 0; i < c.length; i++) {
-			c[alpha[i]-'0']++;
+		for (int i = 0; i < m.length(); i++) {
+			if('A' <= m.charAt(i) && m.charAt(i) <= 'Z') {
+				alpha[m.charAt(i)-'A']++;
+			} else {
+				alpha[m.charAt(i)-'a']++;
+			}
 		}
+		
+		int max = -1;
+		char ch = '?';
+		
+		for (int i = 0; i < 26; i++) {
+			if(alpha[i] > max) {
+				max = alpha[i];
+				ch = (char)(i + 65);
+			} else if (alpha[i] == max) {
+				ch = '?';
+			}
+		}
+		
+		System.out.print(ch);
 	}
 }
