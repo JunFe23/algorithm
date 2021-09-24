deded64ff2e5fe3655dd4a3126dc2fb4d282afe8
JunFe23 <ogum0405@hanmail.net>
2021-09-24 23:56:38 +0900

    2021.09.24

---
+++ src/algorithm/practice/charQ/Q8.java
@@ -0,0 +1,11 @@
+package algorithm.practice.charQ;
+
+import java.util.Scanner;
+
+public class Q8 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+	}
+}
