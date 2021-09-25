baf952755fd0cc6c4e5369b2dd044cec720cd4c3
JunFe23 <ogum0405@hanmail.net>
2021-09-25 16:40:54 +0900

    2021.09.25

--- src/algorithm/practice/charQ/Q7.java
+++ src/algorithm/practice/charQ/Q7.java
@@ -1,5 +1,21 @@
 package algorithm.practice.charQ;
 
-public class Q7 {
+import java.util.Scanner;
 
+public class Q7 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		in.close();
+		
+		a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
+		b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+
+		System.out.println(a>b? a:b);
+		
+		
+	}
 }
--- src/algorithm/practice/charQ/Q8.java
+++ src/algorithm/practice/charQ/Q8.java
@@ -6,6 +6,37 @@ public class Q8 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		String numbers = in.nextLine();
 		
+		int time = 0;
+		
+		for (int i = 0; i < numbers.length(); i++) {
+			switch (numbers.charAt(i)) {
+			case 'A','B','C': time+=3;
+			break;
+			
+			case 'D','E','F': time+=4;
+			break;
+			
+			case 'G','H','I': time+=5;
+			break;
+			
+			case 'J','K','L': time+=6;
+			break;
+			
+			case 'M','N','O': time+=7;
+			break;
+			
+			case 'P','Q','R','S': time+=8;
+			break;
+			
+			case 'T','U','V': time+=9;
+			break;
+			
+			case 'W','X','Y','Z': time+=10;
+			break;
+			}
+		}
+		System.out.println(time);
 	}
 }
